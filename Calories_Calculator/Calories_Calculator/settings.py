925ab327d1a0f46857c188f39be0be0b9b115b2d
MohamedHerz <mohamed.herz96@gmail.com>
2026-01-20 19:43:17 +0300

    connected to postgressSQL

--- Calories_Calculator/Calories_Calculator/settings.py
+++ Calories_Calculator/Calories_Calculator/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,8 +75,11 @@ WSGI_APPLICATION = 'Calories_Calculator.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Calories_Calc_db',
+        'USER': "postgres",
+        'PASSWORD': "19961996",
+        'PORT': '5432'
     }
 }
 
